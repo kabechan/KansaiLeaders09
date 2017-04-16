35d0bb5c7ae034253490f9e96267be4e7f1c4b32
可部谷崇 <kabetanitakashi@ld9-Takashi-Kabetani-no-MacBook-Pro.local>
2017-04-16 14:11:42 +0900

    printを追加

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //眠たい
+        
+        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
